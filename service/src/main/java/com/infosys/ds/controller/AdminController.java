f77e9c5d95bb715b6877ca6c4168a4df5748a7fd
Arthamu <artha.mukherjee@gmail.com>
2019-09-28 11:04:29 +0530

    crossOrigin support

--- service/src/main/java/com/infosys/ds/controller/AdminController.java
+++ service/src/main/java/com/infosys/ds/controller/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,15 +13,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.infosys.ds.model.DeviceProfile;
-import com.infosys.ds.response.CityDataResponse;
 import com.infosys.ds.response.DeviceDataResponse;
 import com.infosys.ds.response.ProductRequest;
 import com.infosys.ds.services.DsAdminServices;
 
 @RestController
 @RequestMapping("api/admin")
+@CrossOrigin("*")
 public class AdminController {
 	
 	@Autowired
